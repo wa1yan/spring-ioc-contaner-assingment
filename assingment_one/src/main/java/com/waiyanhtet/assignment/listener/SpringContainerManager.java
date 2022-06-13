1e6fe1f07d718cd0c7a7af761933ba786f289f9f
waiyanhtet <waiyanhtet@SIP-NB-0049.startinpoint.com>
2022-06-13 22:44:26 +0630

    -add spring container

--- assingment_one/src/main/java/com/waiyanhtet/assignment/listener/SpringContainerManager.java
+++ assingment_one/src/main/java/com/waiyanhtet/assignment/listener/SpringContainerManager.java
@@ -7,23 +7,23 @@ import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.annotation.WebListener;
 
 @WebListener
-public class SpringContainerManager implements ServletContextListener{
+public class SpringContainerManager implements ServletContextListener {
 
 	public static String SPRING_CONTEXT = "spring.context";
 	private GenericXmlApplicationContext springContext;
-	
+
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
 		System.out.println("Initialized IOC container");
-			
+
 		springContext = new GenericXmlApplicationContext("classpath:application.xml");
-				
-		if(null != springContext) {
+
+		if (null != springContext) {
 			sce.getServletContext().setAttribute(SPRING_CONTEXT, springContext);
 		}
-			
+
 	}
-	
+
 	@Override
 	public void contextDestroyed(ServletContextEvent sce) {
 		System.out.println("Closed IOC container");
